7040761ad8f2d5c345659ebd9f35705c6882651f
ankudinov aleksander <sasha@ankudinov.org.ua>
2023-08-16 21:01:05 +0300

    first commit

--- Sources/LAUnity/LAUnity.swift
+++ Sources/LAUnity/LAUnity.swift
@@ -1,6 +1,106 @@
-public struct LAUnity {
-    public private(set) var text = "Hello, World!"
+//
+//  Unity.swift
+//  projectWithModule
+//
+//  Created by ankudinov aleksandr on 28.07.2023.
+//
 
-    public init() {
+import Foundation
+import UIKit
+
+#if canImport(UnityFramework)
+import UnityFramework
+
+public class LAUnity: UIResponder, UIApplicationDelegate {
+
+    static let shared = LAUnity()
+
+    private let dataBundleId: String = "com.unity3d.framework"
+    private let frameworkPath: String = "/Frameworks/UnityFramework.framework"
+
+    private var ufw : UnityFramework?
+    private var hostMainWindow : UIWindow?
+
+    private var isInitialized: Bool {
+        ufw?.appController() != nil
+    }
+
+    public func show(in hostMainWindow: UIWindow?) {
+        self.setHostMainWindow(hostMainWindow)
+        if isInitialized {
+            showWindow()
+        } else {
+            initWindow()
+        }
+    }
+    
+    public func hide() {
+        ufw?.unloadApplication()
+    }
+
+    private func setHostMainWindow(_ hostMainWindow: UIWindow?) {
+        self.hostMainWindow = hostMainWindow
+    }
+   
+    private func initWindow() {
+        if isInitialized {
+            showWindow()
+            return
+        }
+
+        guard let ufw = loadUnityFramework() else {
+            print("ERROR: Was not able to load Unity")
+            return unloadWindow()
+        }
+
+        self.ufw = ufw
+        ufw.setDataBundleId(dataBundleId)
+        ufw.register(self)
+        ufw.runEmbedded(
+            withArgc: CommandLine.argc,
+            argv: CommandLine.unsafeArgv,
+            appLaunchOpts: nil
+        )
+    }
+
+    private func showWindow() {
+        if isInitialized {
+            ufw?.showUnityWindow()
+        }
+    }
+
+    private func unloadWindow() {
+        if isInitialized {
+            ufw?.unloadApplication()
+        }
+    }
+
+    private func loadUnityFramework() -> UnityFramework? {
+        let bundlePath: String = Bundle.main.bundlePath + frameworkPath
+
+        let bundle = Bundle(path: bundlePath)
+        if bundle?.isLoaded == false {
+            bundle?.load()
+        }
+
+        let ufw = bundle?.principalClass?.getInstance()
+        if ufw?.appController() == nil {
+            let machineHeader = UnsafeMutablePointer<MachHeader>.allocate(capacity: 1)
+            machineHeader.pointee = _mh_execute_header
+
+            ufw?.setExecuteHeader(machineHeader)
+        }
+        return ufw
+    }
+    
+}
+
+extension LAUnity: UnityFrameworkListener {
+    public func unityDidUnload(_ notification: Notification!) {
+        ufw?.unregisterFrameworkListener(self)
+        ufw = nil
+        hostMainWindow?.makeKeyAndVisible()
     }
 }
+
+#endif
